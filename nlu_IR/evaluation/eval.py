093bffb6b39816e68934c3295e4c3aee2d5bf351
michaelmoju <michaelmoju@gmail.com>
2020-05-19 16:17:32 +0800

    debug cosqa indexer to elminate duplicate documents. Add evaluation functions.

---
+++ nlu_IR/evaluation/eval.py
@@ -0,0 +1,37 @@
+def eval_sp(golds, predictions):
+    metrics = {'sp_em': 0, 'sp_prec': 0, 'sp_recall': 0, 'sp_f1': 0}
+    
+    assert len(golds) == len(predictions)
+    
+    for gold, pred in zip(golds, predictions):
+        _update_sp(metrics, gold, pred)
+        
+    N = len(golds)
+    for k in metrics.keys():
+        metrics[k] /= N
+        metrics[k] = round(metrics[k], 3)
+    print(metrics)
+    return metrics
+
+
+def _update_sp(metrics, gold, pred):
+    tp, fp, fn = 0, 0, 0
+        
+    for p in pred:
+        if p in gold:
+            tp += 1
+        else:
+            fp += 1
+    for g in gold:
+        if g not in pred:
+            fn += 1
+    precision = 1.0 * tp / (tp + fp) if tp + fp > 0 else 0.0
+    recall = 1.0 * tp / (tp + fn) if tp + fn > 0 else 0.0
+    f1 = 2 * precision * recall / (precision + recall) if precision + recall > 0 else 0.0
+    em = 1.0 if fp + fn == 0 else 0.0
+    metrics['sp_em'] += em
+    metrics['sp_f1'] += f1
+    metrics['sp_prec'] += precision
+    metrics['sp_recall'] += recall
+    
+    return precision, recall, f1
--- nlu_IR/lucene_search/cosQA_search.py
+++ nlu_IR/lucene_search/cosQA_search.py
@@ -21,187 +21,193 @@ mySimilarity = BM25Similarity
 
 
 def main():
-	import argparse
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-llv',
-	                    default='INFO',
-	                    help='Logging level')
-	parser.add_argument('-log',
-	                    default=None,
-	                    help='Output log file')
-	parser.add_argument('-cmd',
-	                    required=True,
-	                    help='Command: {index_all} for indexing the raw documents, {test_search} for searching')
-	parser.add_argument('-doc_path',
-	                    default=config.COS_WIKI,
-	                    help='/path/to/cosQA/doc')
-	
-	args = parser.parse_args()
-	
-	myLogFormat = '%(asctime)s ***%(levelname)s*** [%(name)s:%(lineno)s] - %(message)s'
-	logging.basicConfig(level=str2llv(args.llv), format=myLogFormat, datefmt='%Y/%m/%d %H:%M:%S')
-	if args.log:
-		myhandlers = log_w(args.log)
-		logger.addHandler(myhandlers)
-		logger.log(100, ' '.join(sys.argv))
-	else:
-		logger.log(100, ' '.join(sys.argv))
-	
-	if args.cmd == 'index_all':
-		index_all(args.doc_path)
-	
-	elif args.cmd == 'test_search':
-		test_search()
-		
-	else:
-		raise ValueError('Command {} not defined!'.format(args.cmd))
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-llv',
+                        default='INFO',
+                        help='Logging level')
+    parser.add_argument('-log',
+                        default=None,
+                        help='Output log file')
+    parser.add_argument('-cmd',
+                        required=True,
+                        help='Command: {index_all} for indexing the raw documents, {test_search} for searching')
+    parser.add_argument('-doc_path',
+                        default=config.COS_WIKI,
+                        help='/path/to/cosQA/doc')
+
+    args = parser.parse_args()
+
+    myLogFormat = '%(asctime)s ***%(levelname)s*** [%(name)s:%(lineno)s] - %(message)s'
+    logging.basicConfig(level=str2llv(args.llv), format=myLogFormat, datefmt='%Y/%m/%d %H:%M:%S')
+    if args.log:
+        myhandlers = log_w(args.log)
+        logger.addHandler(myhandlers)
+        logger.log(100, ' '.join(sys.argv))
+    else:
+        logger.log(100, ' '.join(sys.argv))
+
+    if args.cmd == 'index_all':
+        index_all(args.doc_path)
+
+    elif args.cmd == 'test_search':
+        test_search()
+
+    else:
+        raise ValueError('Command {} not defined!'.format(args.cmd))
 
 
 def test_search():
-	# query to be search
-	test_query = 'what movies did Temuera Morrison act in?'
-	
-	# instantiate a searcher, 'en' for English and 'zh' for Chinese
-	mySearcher = CosQASearcher('en')
-	
-	"""
-	search by calling searcher.search(query, top_n)
-	return:
-	A list of tuple(did, title_en, content, score)
-	"""
-	
-	ret_docs = mySearcher.search(query_text=test_query, top_n=1)
-	print(ret_docs)
-	
-	# remember to close the searcher
-	mySearcher.close()
+    # query to be search
+    test_query = 'what movies did Temuera Morrison act in?'
+
+    # instantiate a searcher, 'en' for English and 'zh' for Chinese
+    mySearcher = CosQASearcher('en')
+
+    """
+    search by calling searcher.search(query, top_n)
+    return:
+    A list of tuple(did, title_en, content, score)
+    """
+
+    ret_docs = mySearcher.search(query_text=test_query, top_n=1)
+    print(ret_docs)
+
+    # remember to close the searcher
+    mySearcher.close()
 
 
 class CosQAIndexer:
-	def __init__(self, lang):
-		lucene.initVM()
-		
-		if lang == 'zh':
-			logger.info("index directory:{}".format(config.IDX_COS_ZH))
-			indexDir = SimpleFSDirectory(Paths.get(str(config.IDX_COS_ZH)))
-			analyzer = SmartChineseAnalyzer()
-		elif lang == 'en':
-			logger.info("index directory:{}".format(config.IDX_COS_EN))
-			indexDir = SimpleFSDirectory(Paths.get(str(config.IDX_COS_EN)))
-			analyzer = EnglishAnalyzer()
-		else:
-			raise ValueError('lang should be "zh" or "en", {} is invalid!'.format(lang))
-		writerConfig = IndexWriterConfig(analyzer)
-		writerConfig.setSimilarity(mySimilarity())
-		logger.debug('writer similarity func: {}'.format(writerConfig.getSimilarity()))
-		writer = IndexWriter(indexDir, writerConfig)
-		self.writer = writer
-	
-	def add(self, did, title_en, content):
-		doc = Document()
-		doc.add(StringField("did", did, Field.Store.YES))
-		doc.add(StringField("title_en", title_en, Field.Store.YES))
-		doc.add(TextField("content", content, Field.Store.YES))
-		self.writer.addDocument(doc)
-	
-	def close(self):
-		self.writer.close()
+    def __init__(self, lang):
+        lucene.initVM()
+
+        if lang == 'zh':
+            logger.info("index directory:{}".format(config.IDX_COS_ZH))
+            indexDir = SimpleFSDirectory(Paths.get(str(config.IDX_COS_ZH)))
+            analyzer = SmartChineseAnalyzer()
+        elif lang == 'en':
+            logger.info("index directory:{}".format(config.IDX_COS_EN))
+            indexDir = SimpleFSDirectory(Paths.get(str(config.IDX_COS_EN)))
+            analyzer = EnglishAnalyzer()
+        else:
+            raise ValueError('lang should be "zh" or "en", {} is invalid!'.format(lang))
+        writerConfig = IndexWriterConfig(analyzer)
+        writerConfig.setSimilarity(mySimilarity())
+        logger.debug('writer similarity func: {}'.format(writerConfig.getSimilarity()))
+        writer = IndexWriter(indexDir, writerConfig)
+        self.writer = writer
+
+    def add(self, did, title_en, content):
+        doc = Document()
+        doc.add(StringField("did", did, Field.Store.YES))
+        doc.add(StringField("title_en", title_en, Field.Store.YES))
+        doc.add(TextField("content", content, Field.Store.YES))
+        self.writer.addDocument(doc)
+
+    def close(self):
+        self.writer.close()
 
 
 class CosQASearcher:
-	def __init__(self, lang):
-		lucene.initVM()
-		
-		if lang == 'zh':
-			indexDir = SimpleFSDirectory(Paths.get(str(config.IDX_COS_ZH)))
-			analyzer = SmartChineseAnalyzer()
-		elif lang == 'en':
-			indexDir = SimpleFSDirectory(Paths.get(str(config.IDX_COS_EN)))
-			analyzer = EnglishAnalyzer()
-		else:
-			raise ValueError('lang should be "zh" or "en", {} is invalid!'.format(lang))
-		
-		self.reader = DirectoryReader.open(indexDir)
-		self.searcher = IndexSearcher(self.reader)
-		self.searcher.setSimilarity(mySimilarity())
-		self.analyzer = analyzer
-		logger.debug('search similarity func: {}'.format(self.searcher.getSimilarity()))
-	
-	def search(self, query_text, top_n=1):
-		query_text = query_text.strip()
-		# query = QueryParser("content", self.analyzer).parse(QueryParser.escape(query_text.strip()))
-		query = QueryParser("content", self.analyzer).parse(query_text)
-		scoreDocs = self.searcher.search(query, top_n).scoreDocs
-		
-		out_list = []
-		for scoreDoc in scoreDocs:
-			docIndex = scoreDoc.doc
-			doc = self.searcher.doc(docIndex)
-			log_debug(doc, logger)
-			log_debug(self.searcher.explain(query, docIndex), logger)
-			
-			out_list.append((doc['did'], doc['title_en'], doc['content'], scoreDoc.score))
-		return out_list
-	
-	def close(self):
-		self.reader.close()
+    def __init__(self, lang):
+        lucene.initVM()
+
+        if lang == 'zh':
+            indexDir = SimpleFSDirectory(Paths.get(str(config.IDX_COS_ZH)))
+            analyzer = SmartChineseAnalyzer()
+        elif lang == 'en':
+            indexDir = SimpleFSDirectory(Paths.get(str(config.IDX_COS_EN)))
+            analyzer = EnglishAnalyzer()
+        else:
+            raise ValueError('lang should be "zh" or "en", {} is invalid!'.format(lang))
+
+        self.reader = DirectoryReader.open(indexDir)
+        self.searcher = IndexSearcher(self.reader)
+        self.searcher.setSimilarity(mySimilarity())
+        self.analyzer = analyzer
+        logger.debug('search similarity func: {}'.format(self.searcher.getSimilarity()))
+
+    def search(self, query_text, top_n=1):
+        query_text = query_text.strip()
+        # query = QueryParser("content", self.analyzer).parse(QueryParser.escape(query_text.strip()))
+        query = QueryParser("content", self.analyzer).parse(query_text)
+        scoreDocs = self.searcher.search(query, top_n).scoreDocs
+
+        out_list = []
+        for scoreDoc in scoreDocs:
+            docIndex = scoreDoc.doc
+            doc = self.searcher.doc(docIndex)
+            log_debug(doc, logger)
+            log_debug(self.searcher.explain(query, docIndex), logger)
+
+            out_list.append((doc['did'], doc['title_en'], doc['content'], scoreDoc.score))
+        return out_list
+
+    def close(self):
+        self.reader.close()
 
 
 def index_all(doc_path):
-	def get_content(doc):
-		DID_URL_RGX = re.compile(r'https://www.wikidata.org/wiki/(Q.*)')
-		
-		title_en = doc['title']['en']
-		assert title_en
-		url = doc['ref']['en']['url']
-	
-		if not doc['ref']['en']['wikidata_url']:
-			return None
-		
-		did = DID_URL_RGX.findall(doc['ref']['en']['wikidata_url'])[0]
-		
-		content_en = doc['summary']['en']
-		content_zh = doc['summary']['zh']
-		
-		if not content_en:
-			return None
-		if not content_zh:
-			return None
-		
-		return did, title_en, url, content_en, content_zh
-	
-	raw_fps = list_fps(doc_path, 'json')
-	logger.info("Read {} files".format(len(raw_fps)))
-	
-	# 	assert not os.path.exists(config.IDX_COS_EN), "{} already exists!".format(config.IDX_COS_EN)
-	# 	assert not os.path.exists(config.IDX_COS_ZH), "{} already exists!".format(config.IDX_COS_ZH)
-	
-	new_dir(config.IDX_COS_EN)
-	new_dir(config.IDX_COS_ZH)
-	myIndexer_en = CosQAIndexer("en")
-	myIndexer_zh = CosQAIndexer("zh")
-	
-	try:
-		doc_num = 0
-		logger.info("Start indexing...")
-		for fp in tqdm(raw_fps):
-			docs = json_load(fp)
-			for doc in docs:
-				if not get_content(doc):
-					continue
-				else:
-					did, title_en, url, content_en, content_zh = get_content(doc)
-				myIndexer_en.add(did, title_en, content_en)
-				myIndexer_zh.add(did, title_en, content_zh)
-				doc_num += 1
-		logger.info("Indexed {} docs.".format(doc_num))
-		myIndexer_en.close()
-		myIndexer_zh.close()
+    def get_content(doc):
+        DID_URL_RGX = re.compile(r'https://www.wikidata.org/wiki/(Q.*)')
 
-	finally:
-		myIndexer_en.close()
-		myIndexer_zh.close()
+        title_en = doc['title']['en']
+        assert title_en
+        url = doc['ref']['en']['url']
 
+        if not doc['ref']['en']['wikidata_url']:
+            return None
+
+        did = DID_URL_RGX.findall(doc['ref']['en']['wikidata_url'])[0]
+
+        content_en = doc['summary']['en']
+        content_zh = doc['summary']['zh']
+
+        if not content_en:
+            return None
+        if not content_zh:
+            return None
+
+        return did, title_en, url, content_en, content_zh
+
+    raw_fps = list_fps(doc_path, 'json')
+    logger.info("Read {} files".format(len(raw_fps)))
+
+    # 	assert not os.path.exists(config.IDX_COS_EN), "{} already exists!".format(config.IDX_COS_EN)
+    # 	assert not os.path.exists(config.IDX_COS_ZH), "{} already exists!".format(config.IDX_COS_ZH)
+
+    new_dir(config.IDX_COS_EN)
+    new_dir(config.IDX_COS_ZH)
+    myIndexer_en = CosQAIndexer("en")
+    myIndexer_zh = CosQAIndexer("zh")
+
+    try:
+        doc_num = 0
+        logger.info("Start indexing...")
+
+        indexed_dids = []
+        for fp in tqdm(raw_fps):
+            docs = json_load(fp)
+            for doc in docs:
+                if not get_content(doc):
+                    continue
+                else:
+                    did, title_en, url, content_en, content_zh = get_content(doc)
+                if did in indexed_dids:
+                    continue
+                else:
+                    myIndexer_en.add(did, title_en, content_en)
+                    myIndexer_zh.add(did, title_en, content_zh)
+                    indexed_dids.append(did)
+
+                    doc_num += 1
+        logger.info("Indexed {} docs.".format(doc_num))
+        myIndexer_en.close()
+        myIndexer_zh.close()
+
+    finally:
+        myIndexer_en.close()
+        myIndexer_zh.close()
 
 if __name__ == '__main__':
-	main()
+    main()
